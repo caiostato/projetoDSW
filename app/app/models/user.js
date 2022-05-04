f732d85a7301ca64b9a50a5b6a52b5d45cd041c8
caiostato <zombieiron9@gmail.com>
2022-05-04 12:00:46 -0300

    login 'n signin auth

--- app/app/models/user.js
+++ app/app/models/user.js
@@ -1,12 +1,17 @@
 module.exports = {
     authLogin : function(user,connection, callback){
-        let sql = `select count() from user where user_email="${user.email}" and user_password="${user.password}"`
+        let sql = `select user_id from user where user_email="${user.email}" and user_password="${user.password}";`
         connection.query(sql,callback)
     },
     authSignin : function(user,connection, callback){
-        let sql = `INSERT INTO user (user_name,user_password,user_email,user_date,user_addr,user_img)
-            values ("${user.nome}","${user.password}","${user.email}","${user.date}","${user.addr}","${user.img}")`
+
+        console.log(user)
+        let sql = `INSERT INTO user (user_name,user_password,user_email,user_date,user_addr,user_img) VALUES ("${user.nome}","${user.password}","${user.email}","${user.date}","${user.address}","${user.img}")`
         connection.query(sql,callback)
-    }
+    },
+    checkId : function(user,connection, callback){
+        let sql = `select user_id from user where user_email="${user.email}" and user_password="${user.password}";`
+        connection.query(sql,callback)
+    },
 
 }
