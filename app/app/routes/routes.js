819653e9c6d6090b926d089a410dae619a006458
caiostato <zombieiron9@gmail.com>
2022-05-04 21:14:57 -0300

    routes

--- app/app/routes/routes.js
+++ app/app/routes/routes.js
@@ -8,7 +8,6 @@ module.exports = {
         app.get('/', (req,res)=>{
         controller.listar(app,req,res)
         })
-
     },
     error: function(app){
 
@@ -17,7 +16,6 @@ module.exports = {
         app.get('/error', (req,res)=>{
         controller.loadPage(app,req,res)
         })
-
     },
     pontos: function(app){
 
@@ -25,18 +23,22 @@ module.exports = {
         const { check, validationResult} = require('express-validator')
 
         //ROTA CARREGAR CADASTRAR PONTO PAGE
-        app.get('/pontos/cadastrar', (req,res)=>{
+        app.get('/ponto/cadastrar', (req,res)=>{
         controller.loadPage(app,req,res)
         }),
-        
         //ROTA ENVIAR DADOS SOBRE CADASTRO DO PONTO
         app.post('/pontos/cadastrar/save',[
-            //checks
+            check('nome', 'Nome muito curto').isLength({min:5}),
+            check('cep', 'CEP com tamanho invalido').isLength({min:9,max:9}),
+            check('address', 'Endereço muito curto').isLength({ min:8 }),
+            check('bairro', 'Bairro muito curto').isLength({ min:5 }),
+            check('cidade', 'Cidade muito curto').isLength({ min:5}),
+            check('estado', 'Estado muito curto').isLength({ min:2,max:2}),
+            check('img','Não é URL').isURL(),
         ], (req,res)=>{
             const errors = validationResult(req)
             controller.savePonto(app,req,res,errors)
         })
-
     },
     user: function(app){
 
@@ -47,17 +49,14 @@ module.exports = {
         app.get('/user/login', (req,res)=>{
         controller.loadLogin(app,req,res)
         }),
-
         //ROTA SIGN IN USER
         app.get('/user/signin', (req,res)=>{
             controller.loadSignin(app,req,res)
         }),
-        
         //ROTA AUTH LOGIN
         app.post('/user/login/auth', (req,res)=>{
             controller.authLogin(app,req,res)
         }),
-
         //ROTA AUTH SIGN IN
         app.post('/user/signin/auth',[
             check('nome', 'Nome muito curto').isLength({min:5}),
@@ -72,7 +71,6 @@ module.exports = {
             const errors = validationResult(req)
             controller.authSignin(app,req,res,errors)
         })
-
     },
 }
 
